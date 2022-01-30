7c0a150b0254765f128986e4394fe8eb26a4a661
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-01-30 15:32:26 -0300

    :construction: movendo codigo card
    movendo codigo card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -39,18 +39,17 @@ class Card extends React.Component {
         </div>
         <div id="box-white">
           <div id="box-green">
-            a
+            <h1 data-testid="name-card">{ cardName }</h1>
+            <img src={ cardImage } alt={ cardName } data-testid="image-card" />
+            <p data-testid="description-card">{ cardDescription }</p>
+            <p data-testid="attr1-card">{ cardAttr1 }</p>
+            <p data-testid="attr2-card">{ cardAttr2 }</p>
+            <p data-testid="attr3-card">{ cardAttr3 }</p>
+            <p data-testid="rare-card">{ cardRare }</p>
+            { cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p> }
+            { renderDelete }
           </div>
         </div>
-        <h1 data-testid="name-card">{ cardName }</h1>
-        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
-        <p data-testid="description-card">{ cardDescription }</p>
-        <p data-testid="attr1-card">{ cardAttr1 }</p>
-        <p data-testid="attr2-card">{ cardAttr2 }</p>
-        <p data-testid="attr3-card">{ cardAttr3 }</p>
-        <p data-testid="rare-card">{ cardRare }</p>
-        { cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p> }
-        { renderDelete }
       </div>
     );
   }
